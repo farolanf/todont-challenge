f3e01d5f41d5f1c2aa07e2d2618f68cc876d1663
Farolan Faisal <farolanfaisal@gmail.com>
2023-12-25 16:11:37 +0000

    chore:  update deploy script

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,18 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    eosevm_testnet: {
+      url: "https://api.testnet.evm.eosnetwork.com/",
+      accounts: [process.env.PRIVATE_KEY!]
+    }
+  },
 };
 
 export default config;
